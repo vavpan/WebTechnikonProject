cdaf59aa63a9936d39099e1b490fbb34c7373b5c
vavpan <v4projectvps@gmail.com>
2023-01-06 18:21:05 +0200

    Security classed added

---
+++ src/main/java/com/mycompany/webtechnikonproject/security/AuthenticationFilter.java
@@ -0,0 +1,95 @@
+package com.mycompany.webtechnikonproject.security;
+
+
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ResourceInfo;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+@Provider
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    @Context
+    private ResourceInfo resourceInfo;
+
+    private static final String AUTHORIZATION_PROPERTY = "Authorization";
+    private static final String AUTHENTICATION_SCHEME = "Basic";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        Method method = resourceInfo.getResourceMethod();
+        //Access allowed for all
+        if (!method.isAnnotationPresent(PermitAll.class)) {
+            //Access denied for all
+            if (method.isAnnotationPresent(DenyAll.class)) {
+                requestContext.abortWith(Response.status(Response.Status.FORBIDDEN)
+                        .entity("Access blocked for all users !!").build());
+                return;
+            }
+
+            //Get request headers
+            final MultivaluedMap<String, String> headers = requestContext.getHeaders();
+
+            //Fetch authorization header
+            final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
+
+            //If no authorization information present; block access
+            if (authorization == null || authorization.isEmpty()) {
+                requestContext
+                        .abortWith(Response
+                                .status(Response.Status.UNAUTHORIZED)
+                                .entity("You cannot access this resource")
+                                .build());
+                return;
+            }
+
+            //Get encoded username and password
+            final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
+
+            //Decode username and password
+            String usernameAndPassword = new String(Base64.getDecoder().decode(encodedUserPassword.getBytes()));;
+
+            //Split username and password tokens
+            final StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
+            final String username = tokenizer.nextToken();
+            final String password = tokenizer.nextToken();
+
+            //Verify user access
+            if (method.isAnnotationPresent(RolesAllowed.class)) {
+                RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
+                Set<String> rolesSetForTheResource = new HashSet<>(Arrays.asList(rolesAnnotation.value()));
+
+                //Is user valid?
+                if (!isUserAllowed(username, password, rolesSetForTheResource)) {
+                    requestContext.abortWith(Response
+                            .status(Response.Status.UNAUTHORIZED)
+                            .entity("You cannot access this resource")
+                            .build());
+                    return;
+                }
+            }
+        }
+    }
+
+    private boolean isUserAllowed(final String username, final String password, final Set<String> rolesSetExpected) {
+        boolean isAllowed = false;
+
+        if (username.equals("admin") && password.equals("password")) {
+            String userRole = "ADMIN"; // the role of the username, password
+            if (rolesSetExpected.contains(userRole)) {
+                isAllowed = true;
+            }
+        }
+        return isAllowed;
+    }
+
+}
---
+++ src/main/java/com/mycompany/webtechnikonproject/security/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.mycompany.webtechnikonproject.security;
+
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author iracl
+ */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
