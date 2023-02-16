4c814428ddf1b3595c660eef62309ece6484deaf
vavpan <v4projectvps@gmail.com>
2023-02-16 13:35:47 +0200

    initialization authentication changed

--- src/main/java/com/mycompany/webtechnikonproject/resources/InitResource.java
+++ src/main/java/com/mycompany/webtechnikonproject/resources/InitResource.java
@@ -4,37 +4,35 @@
  */
 package com.mycompany.webtechnikonproject.resources;
 
-import com.mycompany.webtechnikonproject.model.PropertyOwner;
 import com.mycompany.webtechnikonproject.services.IoServices;
-import com.mycompany.webtechnikonproject.services.OwnerService;
+import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import java.util.List;
 
 /**
  *
- * @author ADMIN
+ * @author ADMIN 
  */
 @Path("/initialization")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class InitResource {
-    
+
     @Inject
     private IoServices ioServices;
-    
+
     @GET
+    @PermitAll 
     public void init() {
-        
         ioServices.readOwnersCsv("C:\\Users\\vavil\\Documents\\NetBeansProjects\\WebTechnikonProject\\owners.csv");
         ioServices.readPropertyCsv("C:\\Users\\vavil\\Documents\\NetBeansProjects\\WebTechnikonProject\\property.csv");
         ioServices.readRepairCsv("C:\\Users\\vavil\\Documents\\NetBeansProjects\\WebTechnikonProject\\repairs.csv");
         ioServices.relationshipsBetweenObjects();
-        
+
     }
-    
+
 }
